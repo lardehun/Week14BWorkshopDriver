9efe402c68409579685d69ed150b077577810ed1
szmate <larde15@gmail.com>
2016-04-18 23:29:54 +0200

    task upload

---
+++ Week14BWorkshopMondayDriver/Week14BWorkshopMondayDriver/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Week14BWorkshopMondayDriver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.driverLabel = new System.Windows.Forms.Label();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.driverComboBox = new System.Windows.Forms.ComboBox();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.dirListBox = new System.Windows.Forms.ListBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // driverLabel
+            // 
+            this.driverLabel.AutoSize = true;
+            this.driverLabel.Location = new System.Drawing.Point(12, 34);
+            this.driverLabel.Name = "driverLabel";
+            this.driverLabel.Size = new System.Drawing.Size(35, 13);
+            this.driverLabel.TabIndex = 0;
+            this.driverLabel.Text = "Driver";
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(12, 61);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(29, 13);
+            this.pathLabel.TabIndex = 1;
+            this.pathLabel.Text = "Path";
+            // 
+            // driverComboBox
+            // 
+            this.driverComboBox.FormattingEnabled = true;
+            this.driverComboBox.Location = new System.Drawing.Point(53, 31);
+            this.driverComboBox.Name = "driverComboBox";
+            this.driverComboBox.Size = new System.Drawing.Size(343, 21);
+            this.driverComboBox.TabIndex = 2;
+            this.driverComboBox.SelectedIndexChanged += new System.EventHandler(this.driverComboBox_SelectedIndexChanged);
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(53, 58);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.ReadOnly = true;
+            this.pathTextBox.Size = new System.Drawing.Size(343, 20);
+            this.pathTextBox.TabIndex = 3;
+            // 
+            // dirListBox
+            // 
+            this.dirListBox.FormattingEnabled = true;
+            this.dirListBox.Location = new System.Drawing.Point(15, 92);
+            this.dirListBox.Name = "dirListBox";
+            this.dirListBox.Size = new System.Drawing.Size(381, 160);
+            this.dirListBox.TabIndex = 4;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(15, 256);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 5;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(320, 256);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 291);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.dirListBox);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.driverComboBox);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.driverLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label driverLabel;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.ComboBox driverComboBox;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.ListBox dirListBox;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ Week14BWorkshopMondayDriver/Week14BWorkshopMondayDriver/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week14BWorkshopMondayDriver
+{
+    public partial class Form1 : Form
+    {
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+        public Form1()
+        {
+            InitializeComponent();
+            driverComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            foreach (DriveInfo driver in allDrives)
+            {
+                driverComboBox.Items.Add(driver.Name.ToString() + "    "+ driver.DriveType.ToString());
+            }
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            if (dirListBox.SelectedIndex < 0 || !dirListBox.SelectedItem.ToString().EndsWith(".txt"))
+            {
+                DirectoryInfo selectedDirectory = new DirectoryInfo(pathTextBox.Text + dirListBox.SelectedItem.ToString());
+                pathTextBox.Text = pathTextBox.Text + dirListBox.SelectedItem.ToString();
+                dirListBox.Items.Clear();
+                foreach (DirectoryInfo dir in selectedDirectory.GetDirectories())
+                {
+                    dirListBox.Items.Add(@"\" + dir.Name);
+                }
+                foreach (FileInfo file in selectedDirectory.GetFiles())
+                {
+                    if (file.Name.EndsWith(".txt"))
+                    {
+                        dirListBox.Items.Add(@"\" + file.Name);
+                    }
+                }
+            }
+            else
+            {
+                string text = File.ReadAllText(pathTextBox.Text + dirListBox.SelectedItem.ToString());
+                MessageBox.Show(text);
+            }
+
+        }
+
+        private void driverComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dirListBox.Items.Clear();
+            DriveInfo selectedDriver = allDrives[driverComboBox.SelectedIndex];
+            pathTextBox.Text = selectedDriver.RootDirectory.ToString();
+            if (selectedDriver.DriveType != DriveType.CDRom)
+            {
+                DirectoryInfo selectedDriverDirectory = new DirectoryInfo(selectedDriver.RootDirectory.ToString());
+                foreach (DirectoryInfo dir in selectedDriverDirectory.GetDirectories())
+                {
+                    dirListBox.Items.Add(dir.Name);
+                }
+                foreach (FileInfo file in selectedDriverDirectory.GetFiles())
+                {
+                    if (file.Name.EndsWith(".txt"))
+                    {
+                        dirListBox.Items.Add(file.Name);
+                    }
+                }
+                
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            FileInfo deleteFile = new FileInfo(pathTextBox.Text + dirListBox.SelectedItem.ToString());
+            deleteFile.Delete();
+            dirListBox.Items.Remove(dirListBox.SelectedItem);
+            MessageBox.Show("File Deleted.");
+        }
+    }
+}
